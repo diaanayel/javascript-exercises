c8cee211ceb62d9639d95fea117864d112df3818
diaanayel <diaanayel297@gmail.com>
2025-02-20 16:28:51 +0200

    solve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if(a < 0 || b < 0
+  || !Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
 
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+
+  /*
+  let sum = 0;
+  for(let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+  */
+
+  // Arithmetic Series Summation Formula
+  return ((max - min + 1) * (min + max)) / 2;
 };
 
 // Do not edit below this line
