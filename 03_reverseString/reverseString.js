853956944583747b1ec08a4a38f513191fc64788
diaanayel <diaanayel297@gmail.com>
2025-02-20 12:11:48 +0200

    solve reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  const len = str.length;
+  let reversedStr = '';
 
+  for(let i = len - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
+  // if built in functions allowed
+  // return str.split('').reverse().join('');
 };
 
 // Do not edit below this line
