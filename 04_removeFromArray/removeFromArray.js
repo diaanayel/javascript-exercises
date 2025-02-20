cb5fecf003d46bfdab1fb14690a452868259feb7
diaanayel <diaanayel297@gmail.com>
2025-02-20 16:12:32 +0200

    solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  /*
+  for(let i = 0; i < args.length; i++) {
+    while(array.includes(args[i]))
+      array.splice(array.indexOf(args[i]), 1);
+  }
+  return array;
+  */
+
+  const newArray = [];
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
