53800d6d445cca30f6e97d5eae8dc22331c0dbfa
diaanayel <diaanayel297@gmail.com>
2025-02-20 17:18:45 +0200

    solve leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  const isCencury = year % 100 === 0;
+  const isDivisibleBy4 = year % 4 === 0;
+  const isDivisibleBy400 = year % 400 === 0;
 
+  return ((isCencury && isDivisibleBy400)
+       || (isDivisibleBy4 && !isCencury));
 };
 
 // Do not edit below this line
