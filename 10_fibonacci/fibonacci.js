a1c394d81a440f204f37fd26b3f191f7b1b51f7e
diaanayel <diaanayel297@gmail.com>
2025-02-24 20:09:28 +0200

    solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(targetIdx) {
+  let prevOfPrev = 1;
+  let prev = 1;
+  let curr = prev + prevOfPrev;
+  
+  targetIdx = Number.parseInt(targetIdx);
+  
+  if(targetIdx < 0) return "OOPS";
+  if(targetIdx === 0) return 0;
+  if(targetIdx === 1 || targetIdx === 2) return 1;
 
+  for(let i = 3; i < targetIdx; i++) {
+    prevOfPrev = prev;
+    prev = curr;
+    curr = prev + prevOfPrev;
+  }
+  return curr;
 };
 
 // Do not edit below this line
