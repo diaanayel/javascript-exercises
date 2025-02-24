22f01d1a668aeafb5d680d40add30396c0baa377
diaanayel <diaanayel297@gmail.com>
2025-02-24 18:08:27 +0200

    solve palimdromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const _str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  const maxIdx = _str.length - 1;
+  const half = Math.floor(maxIdx / 2);
 
+  for(let i = 0; i < half; i++) {
+    if(!(_str.at(i) === _str.at(maxIdx - i))) return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
