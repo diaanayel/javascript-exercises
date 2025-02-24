67b50508e667d9468641f532e5358e7799a6bca1
diaanayel <diaanayel297@gmail.com>
2025-02-24 17:39:19 +0200

    solve calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return (nums === null || nums.length === 0) ? 0 
+    : nums.reduce((sum, num) => sum += num);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return (nums === null || nums.length === 0) ? 0 
+    : nums.reduce((res, num) => res *= num);
 };
 
-const power = function() {
-	
+const power = function(n, p) {
+  return n ** p;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n === 0 || n === 1) return 1;
+
+  let res = 1;
+  for(let i = 2; i <= n; i++) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
