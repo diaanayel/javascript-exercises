a28181dbb6785ed7e0295dee10b2b8f8ae00093a
diaanayel <diaanayel297@gmail.com>
2025-02-24 20:54:14 +0200

    solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((oldest, curr) => {
+    const oldestAge = calculateAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currAge = calculateAge(curr.yearOfBirth, curr.yearOfDeath);
+    
+    return oldestAge < currAge ? curr : oldest;
+  })
+};
 
+function calculateAge(bd, dd = new Date().getFullYear()) {
+  return dd - bd;
 };
 
 // Do not edit below this line
